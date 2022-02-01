ea254172d6e94435dc7aeebaec7e02c98681f98a
Mariem AMAR <94847182+mariemamarIA26@users.noreply.github.com>
2022-02-01 11:42:24 +0100

    adding files to the repo

---
+++ ui.R
@@ -0,0 +1,51 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+
+library(shiny)
+library(colourpicker)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+    
+    # Application title
+    titlePanel("Premiers pas avec shiny"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30),
+            
+            # input pour la couleur
+            colourInput(inputId = "color", label = "Choix de la couleur :", value = "blue"),
+            
+            # titre du graphique
+            textInput(inputId = "titre", label = "Entrer un titre", value = "Histogramme"),
+            
+            # choix de la variable
+            radioButtons(inputId = "var", label = "Variables : ", choices = colnames(faithful)),
+            
+            # telechargement du graphique
+            downloadLink('download_plot', 'Télécharger le graphique')
+            
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot"),
+            div(textOutput("n_bins"), align = "center"),
+            tags$hr(),
+            # summary
+            verbatimTextOutput("summary"),
+            dataTableOutput("table")
+        )
+    )
+))
